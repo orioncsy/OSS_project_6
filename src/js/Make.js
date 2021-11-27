bc712664ff0c9e6a0a3f4bf300a2a712b7c29c41
wldhks <wldhks302@o365.skku.edu>
2021-11-27 12:58:20 +0900

    Local storage Implementation

--- src/js/Make.js
+++ src/js/Make.js
@@ -112,3 +112,50 @@ function addAnswerItem(answer) {
 window.addEventListener("load", () => {
     addSbjQuestion(); // init with first question
 });
+
+document.querySelectorAll(".btn-success").forEach(btn => {btn.addEventListener('click',() => {
+
+    //문제가 객관식이면 Objective, 주관식이면 Subjective 저장
+    let typeList = document.querySelectorAll('.btn-primary');
+    //문제 저장
+    let contentList = document.querySelectorAll(".content .form-control");
+    //해설 저장
+    let commentList = document.querySelectorAll(".comment .form-control");
+    
+    //주관식 정답 저장
+    let subAnsList = document.querySelectorAll(".answerS");
+    
+    //객관식 정답 저장
+    let choiceList = document.querySelectorAll(".answerObject .choiceList");
+
+    let qNum = typeList.length; //문제 개수 저장
+    let sNum = 0, oNum = 0; // 주관식 개수, 객관식 개수
+    localStorage.setItem('qNum', qNum);
+    for (let i = 0; i < qNum; i++){
+        let question = {}
+        let qType = typeList[i].value
+        question.qType = qType;
+        question.content = contentList[i].value;
+        question.comment = commentList[i].value;
+        if (qType === 'Objective'){
+            let answer = []
+            choiceList[oNum].querySelectorAll(".form-check-input").forEach((node, index) => {
+                if (node.checked){
+                    answer.push(index+1);
+            }})
+            let choice = []
+            choiceList[oNum].querySelectorAll(".form-control").forEach((node) => {
+                choice.push(node.value);
+            })
+            question.answer = answer
+            question.choice = choice
+            oNum+=1;
+        }
+        else{
+            question.answer = subAnsList[sNum].value
+            sNum+=1;
+        }
+        localStorage.setItem(i+1, JSON.stringify(question))
+        //alert(localStorage.getItem(i+1))
+    }
+});})
